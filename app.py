82966765a45399c75155ffef3a597d660c4cce1f
csmurillo <csmurillo00@gmail.com>
2022-05-07 00:15:21 -0700

    function removePattern and removeWhiteSpace added

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
 import re
 
-outputContent=[]
+
 pattern='\(([^)]*)\)[^(]*$'
 
-# open file
-with open('input.txt') as f:
+
+def removePattern(fileName,outputFileName, pattern):
     # remove brackets from the file
-    line = f.readline()
-    mod_str=re.sub(pattern,'',line)
-    outputContent.append(mod_str+'\n')
-    while line:
+    outputContent=[]
+    # open file
+    with open(fileName) as f:
         line = f.readline()
         mod_str=re.sub(pattern,'',line)
         outputContent.append(mod_str+'\n')
+        while line:
+            line = f.readline()
+            mod_str=re.sub(pattern,'',line)
+            outputContent.append(mod_str+'\n')
 
-# write to file
-with open('output.txt','w') as f:
-    for outputLine in outputContent:
-        f.write(outputLine)
+    # write to file
+    with open(outputFileName,'w') as f:
+        for outputLine in outputContent:
+            f.write(outputLine)
 
+def removeWhiteSpace(fileName,outputFileName):
+    outputContent=[]
+    # open file
+    with open(fileName) as f:
+        line = f.readline()
+        outputContent.append(line)
+        while line:
+            line = f.readline()
+
+            if "\n" in line and len(line)==2:
+                print('yes')
+            else:
+                outputContent.append(line)
+
+    print(outputContent)
+
+    # write to file
+    with open(outputFileName,'w') as f:
+        for outputLine in outputContent:
+            f.write(outputLine)
+
+
+# removePattern('input.txt','output.txt',pattern)
+removeWhiteSpace('input.txt','output.txt')
